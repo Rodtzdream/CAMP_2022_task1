b391f9099e6b8168448f3938f8d79f2237926bae
Yura Oliinyk <olijnikura@gmail.com>
2022-10-24 19:40:25 +0300

    Added the option to fill the matrix manually. Added comments.

--- CAMP_2022_task1.cpp
+++ CAMP_2022_task1.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
 
 float FindDeterm(const std::vector<std::vector<int>>& matrix)
 {
 	return (matrix[0][0] * matrix[1][1] * matrix[2][2]) + (matrix[1][0] * matrix[2][1] * matrix[0][2]) + (matrix[0][1] * matrix[1][2] * matrix[2][0]) -
 		(matrix[0][2] * matrix[1][1] * matrix[2][0]) - (matrix[0][0] * matrix[1][2] * matrix[2][1]) - (matrix[0][1] * matrix[1][0] * matrix[2][2]);
-
 }
 
 int main()
 {
-	srand(time(0));
-
 	std::vector<std::vector<int>> matrixA(3, std::vector<int>(3));
 	std::vector<int> matrixB(3);
 	std::vector<float> matrixX(3);
 	int determ = 0;
 
-	std::cout << "Matrix A:" << std::endl;
-	for (auto& el : matrixA)
-		for (auto& tmp : el)
-			tmp = rand() % 10;
-
+	// Fill in matrix A //
+	std::cout << "Fill in matrix A:" << std::endl;
 	for (int i = 0; i < 3; ++i)
 	{
 		for (int j = 0; j < 3; ++j)
-			std::cout << matrixA[j][i] << ' ';
+		{
+			std::cout << "A[" << i << "][" << j << "] = ";
+			std::cin >> matrixA[j][i];
+		}
 		std::cout << std::endl;
 	}
 
-	std::cout << std::endl << "Matrix B:" << std::endl;
-	for (auto& el : matrixB)
+	// Fill in matrix B //
+	std::cout << "Fill in matrix B:" << std::endl;
+	for (int i = 0; i < 3; ++i)
 	{
-		el = rand() % 10;
-		std::cout << el << std::endl;
+		std::cout << "B[" << i << "][" << 0 << "] = ";
+		std::cin >> matrixB[i];
 	}
 
+	// Find determinant of 3x3 matrix //
 	determ = FindDeterm(matrixA);
 
+	// Formation of matrix X //
 	for (int i = 0; i < 3; ++i)
 	{
 		std::swap(matrixA[i], matrixB);
@@ -46,6 +45,7 @@ int main()
 		std::swap(matrixA[i], matrixB);
 	}
 
+	// Print matrix X //
 	std::cout << std::endl << "Matrix X:" << std::endl;
 	for (auto& el : matrixX)
 		std::cout << el << std::endl;
